3cd8404f9b20d2233e6bd046871d2bc438efd8ac
Taisia <tmertz@usc.edu>
2025-04-24 19:25:16 -0700

    adjusted hrefs

--- redesign/src/components/Footer.tsx
+++ redesign/src/components/Footer.tsx
@@ -68,38 +68,38 @@ const Footer: React.FC<FooterProps> = () => {
           <div className="col-span-6 flex flex-col gap-4 md:col-span-3 lg:col-span-2">
             <h3 className="text-[#898683] text-[15.375px] leading-6">Solutions for Distribution</h3>
             <ul className="flex flex-col gap-4">
-              <a className="text-[#625E5B] text-[15.375px] hover:text-bluePrimary" href="/solutions">Order Entry</a>
-              <a className="text-[#625E5B] text-[15.375px] hover:text-bluePrimary" href="/solutions">Voice AI for Calls</a>
-              <a className="text-[#625E5B] text-[15.375px] hover:text-bluePrimary" href="/solutions">Deductions</a>
-              <a className="text-[#625E5B] text-[15.375px] hover:text-bluePrimary" href="/solutions">Cross-sell and Upsell</a>
-              <a className="text-[#625E5B] text-[15.375px] hover:text-bluePrimary" href="/solutions">Churn Analytics</a>
+              <a className="text-[#625E5B] text-[15.375px] hover:text-bluePrimary" href="/animation">Order Entry</a>
+              <a className="text-[#625E5B] text-[15.375px] hover:text-bluePrimary" href="/animation">Voice AI for Calls</a>
+              <a className="text-[#625E5B] text-[15.375px] hover:text-bluePrimary" href="/animation">Deductions</a>
+              <a className="text-[#625E5B] text-[15.375px] hover:text-bluePrimary" href="/animation">Cross-sell and Upsell</a>
+              <a className="text-[#625E5B] text-[15.375px] hover:text-bluePrimary" href="/animation">Churn Analytics</a>
             </ul>
           </div>
           <div className="col-span-6 flex flex-col gap-4 md:col-span-3 lg:col-span-2">
             <h3 className="text-[#898683] text-[15.375px] leading-6">Solutions for Manufacturing</h3>
             <ul className="flex flex-col gap-4">
-              <a className="text-[#625E5B] text-[15.375px] hover:text-bluePrimary" href="/solutions">Sales Order Entry</a>
-              <a className="text-[#625E5B] text-[15.375px] hover:text-bluePrimary" href="/solutions">Quoting</a>
-              <a className="text-[#625E5B] text-[15.375px] hover:text-bluePrimary" href="/solutions">Shipping Notices</a>
-              <a className="text-[#625E5B] text-[15.375px] hover:text-bluePrimary" href="/solutions">Invoice Matching</a>
-              <a className="text-[#625E5B] text-[15.375px] hover:text-bluePrimary" href="/solutions">Bills of Lading</a>
+              <a className="text-[#625E5B] text-[15.375px] hover:text-bluePrimary" href="/animation">Sales Order Entry</a>
+              <a className="text-[#625E5B] text-[15.375px] hover:text-bluePrimary" href="/animation">Quoting</a>
+              <a className="text-[#625E5B] text-[15.375px] hover:text-bluePrimary" href="/animation">Shipping Notices</a>
+              <a className="text-[#625E5B] text-[15.375px] hover:text-bluePrimary" href="/animation">Invoice Matching</a>
+              <a className="text-[#625E5B] text-[15.375px] hover:text-bluePrimary" href="/animation">Bills of Lading</a>
             </ul>
           </div>
           <div className="col-span-6 flex flex-col gap-4 md:col-span-3 lg:col-span-2">
             <h3 className="text-[#898683] text-[15.375px] leading-6">Customers</h3>
             <ul className="flex flex-col gap-4">
-              <a className="text-[#625E5B] text-[15.375px] hover:text-bluePrimary" href="/customers">Customer Stories</a>
-              <a className="text-[#625E5B] text-[15.375px] hover:text-bluePrimary" href="/contact-sales">Contact Sales</a>
+              <a className="text-[#625E5B] text-[15.375px] hover:text-bluePrimary" href="/animation">Customer Stories</a>
+              <a className="text-[#625E5B] text-[15.375px] hover:text-bluePrimary" href="/animation">Contact Sales</a>
             </ul>
           </div>
           <div className="col-span-6 flex flex-col gap-4 md:col-span-3 lg:col-span-2">
             <h3 className="text-[#898683] text-[15.375px] leading-6">Company</h3>
             <ul className="flex flex-col gap-4">
-              <a className="text-[#625E5B] text-[15.375px] hover:text-bluePrimary" href="/about">About</a>
-              <a className="text-[#625E5B] text-[15.375px] hover:text-bluePrimary" href="/blog">Blog &amp; Resources</a>
-              <a className="text-[#625E5B] text-[15.375px] hover:text-bluePrimary" href="/careers">Careers</a>
-              <a className="text-[#625E5B] text-[15.375px] hover:text-bluePrimary" href="/terms-and-conditions">Terms &amp; Conditions</a>
-              <a className="text-[#625E5B] text-[15.375px] hover:text-bluePrimary" href="/privacy-policy">Privacy Policy</a>
+              <a className="text-[#625E5B] text-[15.375px] hover:text-bluePrimary" href="/animation">About</a>
+              <a className="text-[#625E5B] text-[15.375px] hover:text-bluePrimary" href="/animation">Blog &amp; Resources</a>
+              <a className="text-[#625E5B] text-[15.375px] hover:text-bluePrimary" href="/animation">Careers</a>
+              <a className="text-[#625E5B] text-[15.375px] hover:text-bluePrimary" href="/animation">Terms &amp; Conditions</a>
+              <a className="text-[#625E5B] text-[15.375px] hover:text-bluePrimary" href="/animation">Privacy Policy</a>
             </ul>
             
             <h3 className="text-[#898683] text-[15.375px] leading-6 mt-4">Social</h3>
